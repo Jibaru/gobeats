09777380c4a1a6d9aab7b37efcfefe8647d58023
jibaru <ignacioruedaboada@hotmail.com>
2023-10-29 14:59:53 -0500

    feat: update drive player

--- internal/entities/drive_player.go
+++ internal/entities/drive_player.go
@@ -2,30 +2,54 @@ package entities
 
 import (
 	"bytes"
+	"errors"
 	"github.com/ebitengine/oto/v3"
 	"github.com/hajimehoshi/go-mp3"
 	"io"
 	"net/http"
 	"os"
+	"sync"
+	"time"
 )
 
 type DrivePlayer struct {
-	Context       *oto.Context
-	Player        *oto.Player
-	IsDownloading bool
+	context       *oto.Context
+	player        *oto.Player
+	mutex         sync.Mutex
+	CurrentSong   *DriveFile
 	CurrentVolume int
+	isDownloading bool
+	isCanceled    bool
 }
 
-func NewDrivePlayer(context *oto.Context) *DrivePlayer {
+func NewDrivePlayer(
+	context *oto.Context,
+	initialVolume int,
+) (*DrivePlayer, error) {
+	if initialVolume > 100 || initialVolume < 0 {
+		return nil, errors.New("volume must be betweeen 0 and 100")
+	}
+
 	return &DrivePlayer{
 		context,
 		nil,
+		sync.Mutex{},
+		nil,
+		initialVolume,
 		false,
-		50,
-	}
+		false,
+	}, nil
 }
 
-func (p *DrivePlayer) DownloadAndPlay(driveFile DriveFile) error {
+func (p *DrivePlayer) DownloadAndPlay(
+	driveFile DriveFile,
+	onPlaying func(duration int64, song DriveFile),
+	onFinish func(),
+) error {
+	p.Cancel()
+
+	p.CurrentSong = &driveFile
+
 	var fileBytes []byte
 	var err error
 
@@ -35,12 +59,9 @@ func (p *DrivePlayer) DownloadAndPlay(driveFile DriveFile) error {
 			return err
 		}
 	} else {
-		p.IsDownloading = true
-		if err = downloadMP3(driveFile.Url(), driveFile.CachedFilename()); err != nil {
-			p.IsDownloading = false
+		if err = p.downloadMP3(driveFile.Url(), driveFile.CachedFilename()); err != nil {
 			return err
 		}
-		p.IsDownloading = false
 
 		fileBytes, err = os.ReadFile(driveFile.CachedFilename())
 		if err != nil {
@@ -54,14 +75,31 @@ func (p *DrivePlayer) DownloadAndPlay(driveFile DriveFile) error {
 		return err
 	}
 
-	err = p.Stop()
-	if err != nil {
-		return err
-	}
+	p.player = p.context.NewPlayer(decodedMp3)
+	p.player.SetVolume(p.VolumeAsOne())
+	p.player.Play()
+	p.UnCancel()
 
-	p.Player = p.Context.NewPlayer(decodedMp3)
-	p.Player.SetVolume(p.VolumeAsOne())
-	p.Player.Play()
+	var currentTime int64 = 0
+	go func() {
+		for {
+			p.mutex.Lock()
+			if p.isCanceled {
+				p.mutex.Unlock()
+				return
+			}
+			p.mutex.Unlock()
+
+			if p.player.IsPlaying() {
+				currentTime++
+				onPlaying(currentTime, driveFile)
+				time.Sleep(1 * time.Second)
+			} else {
+				onFinish()
+				return
+			}
+		}
+	}()
 
 	return nil
 }
@@ -71,43 +109,52 @@ func (p *DrivePlayer) VolumeAsOne() float64 {
 }
 
 func (p *DrivePlayer) Pause() {
-	if p.Player != nil && p.Player.IsPlaying() {
-		p.Player.Pause()
+	if p.player != nil && p.player.IsPlaying() {
+		p.player.Pause()
 	}
 }
 
 func (p *DrivePlayer) Resume() {
-	if p.Player != nil && !p.Player.IsPlaying() {
-		p.Player.Play()
+	if p.player != nil && !p.player.IsPlaying() {
+		p.player.Play()
 	}
 }
 
+func (p *DrivePlayer) Cancel() {
+	p.mutex.Lock()
+	p.isCanceled = true
+	p.mutex.Unlock()
+	time.Sleep(1 * time.Second)
+}
+
+func (p *DrivePlayer) UnCancel() {
+	p.mutex.Lock()
+	p.isCanceled = false
+	p.mutex.Unlock()
+}
+
 func (p *DrivePlayer) Stop() error {
-	if p.Player != nil {
-		err := p.Player.Close()
+	if p.player != nil {
+		err := p.player.Close()
 		if err != nil {
+			p.player = nil
 			return err
 		}
 	}
 
+	p.player = nil
 	return nil
 }
 
-func (p *DrivePlayer) Close() error {
-	return nil
-	/*err := os.Remove("audio.mp3")
-	if err != nil {
-		return err
-	}
-
-	return nil*/
+func (p *DrivePlayer) IsFinished() bool {
+	return (p.player != nil && !p.player.IsPlaying()) || p.player == nil
 }
 
 func (p *DrivePlayer) IncreaseVolume() {
 	if p.CurrentVolume+1 <= 100 {
 		p.CurrentVolume += 1
-		if p.Player != nil {
-			p.Player.SetVolume(p.VolumeAsOne())
+		if p.player != nil {
+			p.player.SetVolume(p.VolumeAsOne())
 		}
 	}
 }
@@ -115,13 +162,13 @@ func (p *DrivePlayer) IncreaseVolume() {
 func (p *DrivePlayer) DecreaseVolume() {
 	if p.CurrentVolume-1 >= 0 {
 		p.CurrentVolume -= 1
-		if p.Player != nil {
-			p.Player.SetVolume(p.VolumeAsOne())
+		if p.player != nil {
+			p.player.SetVolume(p.VolumeAsOne())
 		}
 	}
 }
 
-func downloadMP3(url, filename string) error {
+func (p *DrivePlayer) downloadMP3(url, filename string) error {
 	resp, err := http.Get(url)
 	if err != nil {
 		return err
